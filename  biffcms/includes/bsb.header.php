567cf552cafa97b971d02851eadba548fb710772
Chris Tankersley <dragonmantank@gmail.com>
2007-03-04 04:28:25 +0000

    Created globals.php and added it to bsb.header.php

---  biffcms/includes/bsb.header.php
+++  biffcms/includes/bsb.header.php
@@ -1,9 +1,31 @@
 <?php
-
+/******************************************************************************
+ * Filename:		bsb.header.php
+ * Description:		Program header file. Includes all of the needed files for
+ * 					BiffCMS
+ * Creation Date:	Uknown (Before 02/24/2007 modifications)
+ * Original Author:	Chris Tankersley (dragonmantank@gmail.com)
+ * 
+ * Custom Modifications:
+ * =====================
+ * MM/DD/YYYY	PRGMR	DESCRIPTION OF CHANGES
+ * ----------	-----	----------------------
+ *
+ * BSB Modifications:
+ * =====================
+ * CRT = Chris Tankersley
+ *
+ * MM/DD/YYYY	PRGMR	DESCRIPTION OF CHANGES
+ * ----------	-----	----------------------
+ * 02/24/2007	CRT		Added 'globals.php'
+ *****************************************************************************/
+ 
 require_once 'biffapi/biffapi.header.php';
+require_once 'globals.php';
 require_once 'programObject.class.php';
 require_once 'module.class.php';
 require_once 'page.class.php';
 require_once 'domain.class.php';
+
 
 ?>
---
+++  biffcms/includes/globals.php
@@ -0,0 +1,35 @@
+<?php
+
+/******************************************************************************
+ * Filename:		globals.php
+ * Description:		This file contains globals that are specific to 
+ *					BiffCMS
+ * Creation Date:	02/24/2007
+ * Original Author:	Chris Tankersley (dragonmantank@gmail.com)
+ * 
+ * Custom Modifications:
+ * =====================
+ *
+ * MM/DD/YYYY	PRGMR	DESCRIPTION OF CHANGES
+ * ----------	-----	----------------------
+ *
+ * BSB Modifications:
+ * =====================
+ * 
+ * MM/DD/YYYY	PRGMR	DESCRIPTION OF CHANGES
+ * ----------	-----	----------------------
+ *****************************************************************************/
+
+// DEFAULT CMS USER GROUPS
+define("CMS_GROUP_SUPER_USER", "CMS Super Users");
+define("CMS_GROUP_ADMINISTRATOR", "CMS Administrator");
+define("CMS_GROUP_DOMAIN_ADMIN", "CMS Domain Administrator");
+define("CMS_GROUP_SITE_ADMIN", "CMS Site Administrator");
+define("CMS_GROUP_PAGE_ADMIN", "CMS Page Administrator");
+define("CMS_GROUP_MODULE_ADMIN", "CMS Module Administrator");
+
+// ============================================================================
+// CUSTOM MODIFICATIONS ARE BELOW
+// ============================================================================
+
+?>
