e644c4adc32cc4b1a8c21f14ad47aec03592ad49
Chris Tankersley <dragonmantank@gmail.com>
2009-01-15 03:26:59 +0000

    Moved core app, crons, libs, and setup files to a biffcms folder Updated bootstrapping to point to the new folder layout

--- www/index.php
+++ www/index.php
@@ -6,13 +6,13 @@ date_default_timezone_set('America/New_York');
 // Directory setup and class loading
 set_include_path(
 	'.' . 
-	PATH_SEPARATOR . '../app' . 
-	PATH_SEPARATOR . '../libs/' . 
-	PATH_SEPARATOR . '../app/models' . 
-	PATH_SEPARATOR . '../app/forms' .
+	PATH_SEPARATOR . '../biffcms/app' . 
+	PATH_SEPARATOR . '../biffcms/libs/' . 
+	PATH_SEPARATOR . '../biffcms/app/models' . 
+	PATH_SEPARATOR . '../biffcms/app/forms' .
 	PATH_SEPARATOR . get_include_path() );
 	
-define('BCMS_MODULE_VIEWPATH', dirname(dirname(__FILE__)) . '/app/cmsModuleViews/');
+define('BCMS_MODULE_VIEWPATH', dirname(dirname(__FILE__)) . '/biffcms/app/cmsModuleViews/');
 
 include 'Zend/Loader.php';
 include 'Tws/Bootstrap.php';
@@ -22,7 +22,7 @@ $bootstrap->startSessions();
 
 // Load configuration
 $environment	= strtolower( array_key_exists('ENVIRONMENT', $_SERVER) ? $_SERVER['ENVIRONMENT'] : 'production' );
-$config 		= new Zend_Config_Ini('../app/config/config.ini', $environment);
+$config 		= new Zend_Config_Ini('config/config.ini', $environment);
 $registry		= Zend_Registry::getInstance();
 $registry->set('config', $config);
 
@@ -35,9 +35,9 @@ Zend_Registry::set('db', $db);
 $bootstrap->throwExceptions(true);
 $bootstrap->registerPlugin('Zend_Controller_Plugin_ErrorHandler');
 $bootstrap->setControllerDirectory(array(
-	'admin'		=> '../app/modules/admin/controllers',
-	'auth'		=> '../app/modules/auth/controllers',
-	'default'	=> '../app/modules/default/controllers',
+	'admin'		=> '../biffcms/app/modules/admin/controllers',
+	'auth'		=> '../biffcms/app/modules/auth/controllers',
+	'default'	=> '../biffcms/app/modules/default/controllers',
 ));
 $bootstrap->setParam('useDefaultControllerAlways', true);
 
@@ -46,7 +46,7 @@ $route	= new Zend_Controller_Router_Route('page/:page', array('module'=>'default
 $bootstrap->addRoute('page', $route);
 
 // Load our layout paths
-Zend_Layout::startMvc( array('layoutPath'	=> '../app/layouts') );
+Zend_Layout::startMvc( array('layoutPath'	=> '../biffcms/app/layouts') );
 
 // Add View Helpers
 $viewRenderer	= Zend_Controller_Action_HelperBroker::getStaticHelper('viewRenderer');
