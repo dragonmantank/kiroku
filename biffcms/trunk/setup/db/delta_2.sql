5f5668ca015c277ca353f1b08d529ea6487c3679
Chris Tankersley <dragonmantank@gmail.com>
2009-05-12 02:34:27 +0000

    delta_2.sql was missing a semicolon between commands, corrected

--- biffcms/trunk/setup/db/delta_2.sql
+++ biffcms/trunk/setup/db/delta_2.sql
@@ -1,2 +1,2 @@
- ALTER TABLE `cms_user_accounts` CHANGE `active` `active` INT( 11 ) NOT NULL DEFAULT '1'
+ ALTER TABLE `cms_user_accounts` CHANGE `active` `active` INT( 11 ) NOT NULL DEFAULT '1';
  ALTER TABLE `cms_user_accounts` CHANGE `user_group` `user_group` INT( 11 ) NOT NULL DEFAULT '1' 
